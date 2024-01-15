7ef3804e53be77edf9ac43ee0c4540ebabb54318
imxey <xeylaxeyla@gmail.com>
2024-01-15 10:25:26 +0700

    .

--- app.py
+++ app.py
@@ -2,38 +2,52 @@ username = "1"
 password = "1"
 
 database_buku = [
-    ["Judul 1", "penulis 1", "Tahun 1"],
-    ["Judul 2", "penulis 2", "Tahun 2"],
-    ["Judul 3", "penulis 3", "Tahun 3"]
-
+    ["Judul 1", "Penulis 1", "Tahun 1"],
+    ["Judul 2", "Penulis 2", "Tahun 2"],
+    ["Judul 3", "Penulis 3", "Tahun 3"]
 ]
+
 def login():
     while True:
-        login_user=input("Enter username : ")
-        login_pass=input("Enter password : ")
-        if login_user==username and login_pass==password:
-            print("Login successful")
+        login_user = input("Masukkan username: ")
+        login_pass = input("Masukkan password: ")
+        if login_user == username and login_pass == password:
+            print("Login berhasil")
             return True
         else:
-            print("Login failed")
+            print("Login gagal")
+
 def lihat_buku():
-    print("Daftar buku perpustakaan: ")
-    for i in range(len(database_buku[0])):
-        print("%s. %s" %([i+1], database_buku[i]))
+    print("Daftar buku perpustakaan:")
+    for i, buku in enumerate(database_buku, 1):
+        print(f"%d. Judul: %s, Penulis: %s, Tahun: %s" %(i, buku[0], buku[1], buku[2]))
+    balik_menu = input("silahkan tekan enter untuk kembali ke menu awal")
+    if balik_menu == "":
+        menu()
 
 def tambah_buku():
-    print("Tambah")
-def hapus_buku(): 
-    print("Hapus")
+    judul_buku = input("Masukkan judul buku yang ingin ditambahkan: ")
+    penulis_buku = input("Masukkan penulis buku yang ingin ditambahkan: ")
+    tahun_buku = input("Masukkan tahun buku yang ingin ditambahkan: ")
+    database_buku.append([judul_buku, penulis_buku, tahun_buku])
+    print("Buku berhasil ditambahkan.")
+    menu()
+
+def hapus_buku():
+    print("Hapus buku")
 
 def peminjaman_buku():
-    print("Peminjaman")
+    print("Peminjaman buku")
+
 def menu():
-    print('1. Lihat buku')
-    print('2. Tambah buku')
-    print('3. Hapus buku dari daftar buku')
+    print("")
+    print("")
+    print("===Menu Perpustakaan===")
+    print('1. Lihat daftar buku')
+    print('2. Tambah buku baru')
+    print('3. Hapus buku dari daftar')
     print('4. Peminjaman buku')
-    pilihan_menu = input('Silahkan Pilih Menu (pilih berdasarkan nomor)')
+    pilihan_menu = input('Silahkan pilih menu (masukkan nomor): ')
     if pilihan_menu == '1':
         lihat_buku()
     elif pilihan_menu == '2':
